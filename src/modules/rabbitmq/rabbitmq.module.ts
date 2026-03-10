c635ba8e2830f8f9c0e33f50c2b764d6d99df908
nurhabibrs <nurhabibrizkisaputro@gmail.com>
2026-03-11 05:42:24 +0700

    fix: update environment configurations for RabbitMQ and remove unused docker-compose file

--- src/modules/rabbitmq/rabbitmq.module.ts
+++ src/modules/rabbitmq/rabbitmq.module.ts
@@ -1,20 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { LoggingProducer } from './logging.producer';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'LOG_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://localhost:5672'],
-          queue: 'logs_queue',
-          queueOptions: {
-            durable: true,
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [
+              configService.get<string>('RABBITMQ_URL') ||
+                'amqp://hris-rabbitmq:5672',
+            ],
+            queue: 'logs_queue',
+            queueOptions: {
+              durable: true,
+            },
           },
-        },
+        }),
       },
     ]),
   ],
